c878f648b5dcf06e237a8eddfa525f3ce7b381c2
Adithya-whjr <71129915+Adithya-whjr@users.noreply.github.com>
2020-11-27 17:52:29 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,30 @@
+class Bob {
+    
+        constructor(x, y) {
+          var options = {
+              isStatic: false ,
+              restitution: 1,
+              friction:0.3,
+              density: 0.8
+          }
+
+
+
+    this.body = Bodies.circle(x, y, 25 , options);
+    this.radius = 25;
+    
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    
+    
+    
+    ellipseMode(RADIUS);
+    
+    fill("silver");
+    ellipse(pos.x,pos.y, this.radius, this.radius);
+   
+  }
+}
